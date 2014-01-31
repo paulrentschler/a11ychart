c022b982fea1ddf7704cd3e4ac208440260096c3
Paul Rentschler <paul@rentschler.ws>
2014-01-31 11:10:51 -0500

    defined a basic jQuery compatible function for creating charts

--- js/a11ycharts.js
+++ js/a11ycharts.js
@@ -4,3 +4,23 @@
  * @author     Paul Rentschler <paul@rentschler.ws>
  * @since      31 January 2014
  */
+
+(function($) {
+    $.fn.a11yChart = function(params)
+    {
+        // merge default and user parameters
+        params = $.extend({
+            type: "horizontal-bar",
+            data: {}
+        }, params);
+
+        this.each(function() {
+            // code goes here to generate the charts
+
+
+        });
+
+        // allow jQuery chaining
+        return this;
+    };
+})(jQuery);
